87294849194dd28f602d5c3d504bd699a11af2ea
Keithrfield <kthrfld@gmail.com>
2023-12-18 10:33:08 -0500

    getting ready for tailwind styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import Employee from './components/Employee'
+import {useState} from 'react';
 
 function App() {
   const showEmployees = true;
+  const [role, setRole] = useState('Dev');
   return (
     <div className="App">
       {showEmployees ? 
         <>
-          <Employee firstname="bill" />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
+        <input type="text" onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
+        }}  />
+          <Employee name="Bill" role="Intern" />
+          <Employee name="Keith" role={role}/>
+          <Employee name="Jill" />
         </>
         :
           <p>You are not permitted to see the employees</p>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,5 +1,10 @@
-function Employee({firstname}) {
-    return <h3 style={{padding:"10px", width:"fit-content", color:"blue", fontSize:"30px", backgroundColor: "lightgrey"}}> Here is an employee {firstname} !</h3>
+function Employee(props) {
+    return (        
+    <>
+        <h3> This is employee {props.name} !</h3>
+        <p>{props.role ? props.role : "No Role"}</p>
+    </>
+    )
 }
 
 export default Employee; 
