3ea33cb5bff4a6aa4ae20733410bf9cdc10b02bb
Keithrfield <kthrfld@gmail.com>
2023-12-19 17:22:33 -0500

    Added modal and styled form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
 import './index.css';
 import Employee from './components/Employee'
 import {useState} from 'react';
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
   const showEmployees = true;
-  const [role, setRole] = useState('Dev');
+  const [employees, setEmployees] = useState(
+    [
+      {
+        name: "Sam", 
+        role: "CEO",
+        img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg"
+      },{
+        name: "Nancy", 
+        role: "Help Desk Associate",
+        img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg"
+      },
+      {
+        name: "Bill", 
+        role: "Sales Associate",
+        img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg"
+      },
+      {
+        name: "Debra", 
+        role: "Accountant",
+        img: "https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg"
+      },
+      {
+        name: "Keith", 
+        role: "Developer",
+        img: "https://images.pexels.com/photos/316680/pexels-photo-316680.jpeg"
+      },
+      {
+        name: "Don", 
+        role: "Manager",
+        img: "https://images.pexels.com/photos/2128807/pexels-photo-2128807.jpeg"
+      },
+      {
+        name: "Valerie", 
+        role: "Software Engineer",
+        img: "https://images.pexels.com/photos/732425/pexels-photo-732425.jpeg"
+      },
+    ]
+  );
   return (
-    <div className="App bg-cyan-300" >
+    <div className="App" >
       {showEmployees ? 
         <>
-        <input type="text" onChange={(e) => {
-            console.log(e.target.value);
-            setRole(e.target.value);
-        }}  />
-          <Employee name="Bill" role="Intern" />
-          <Employee name="Keith" role={role}/>
-          <Employee name="Jill" />
+        
+          <div className="flex flex-wrap justify-center">
+          {employees.map((employee) => {              
+              return(
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />)
+          })}   
+          </div>
         </>
         :
           <p>You are not permitted to see the employees</p>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,10 +1,25 @@
+import EditEmployee from "./EditEmployee";
+
 function Employee(props) {
-    return (        
-    <>
-        <h3> This is employee {props.name} !</h3>
-        <p>{props.role ? props.role : "No Role"}</p>
-    </>
-    )
+  return (
+    <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img
+        className="object-cover h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+        src={props.img}
+        alt="Employee headshot" 
+      />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">{props.name}</p>
+          <p className="text-slate-500 font-medium">
+            {props.role ? props.role : "No Role"}
+          </p>
+        </div>
+        <EditEmployee />
+        
+      </div>
+    </div>
+  );
 }
 
-export default Employee; 
+export default Employee;
