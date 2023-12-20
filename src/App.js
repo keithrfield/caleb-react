485879fa50941ccc36f2a4fa0c4dfd33de5b2026
Keithrfield <kthrfld@gmail.com>
2023-12-20 14:46:32 -0500

    Finished edit modal

--- src/App.js
+++ src/App.js
@@ -1,69 +1,85 @@
-import './index.css';
-import Employee from './components/Employee'
-import {useState} from 'react';
-import {v4 as uuidv4} from 'uuid'
+import "./index.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id === employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
   const showEmployees = true;
-  const [employees, setEmployees] = useState(
-    [
-      {
-        name: "Sam", 
-        role: "CEO",
-        img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg"
-      },{
-        name: "Nancy", 
-        role: "Help Desk Associate",
-        img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg"
-      },
-      {
-        name: "Bill", 
-        role: "Sales Associate",
-        img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg"
-      },
-      {
-        name: "Debra", 
-        role: "Accountant",
-        img: "https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg"
-      },
-      {
-        name: "Keith", 
-        role: "Developer",
-        img: "https://images.pexels.com/photos/316680/pexels-photo-316680.jpeg"
-      },
-      {
-        name: "Don", 
-        role: "Manager",
-        img: "https://images.pexels.com/photos/2128807/pexels-photo-2128807.jpeg"
-      },
-      {
-        name: "Valerie", 
-        role: "Software Engineer",
-        img: "https://images.pexels.com/photos/732425/pexels-photo-732425.jpeg"
-      },
-    ]
-  );
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: "Sam",
+      role: "CEO",
+      img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg",
+    },
+    {
+      id: 2,
+      name: "Nancy",
+      role: "Help Desk Associate",
+      img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg",
+    },
+    {
+      id: 3,
+      name: "Bill",
+      role: "Sales Associate",
+      img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg",
+    },
+    {
+      id: 4,
+      name: "Debra",
+      role: "Accountant",
+      img: "https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg",
+    },
+    {
+      id: 5,
+      name: "Keith",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/316680/pexels-photo-316680.jpeg",
+    },
+    {
+      id: 6,
+      name: "Don",
+      role: "Manager",
+      img: "https://images.pexels.com/photos/2128807/pexels-photo-2128807.jpeg",
+    },
+    {
+      id: 7,
+      name: "Valerie",
+      role: "Software Engineer",
+      img: "https://images.pexels.com/photos/732425/pexels-photo-732425.jpeg",
+    },
+  ]);
   return (
-    <div className="App" >
-      {showEmployees ? 
+    <div className="App">
+      {showEmployees ? (
         <>
-        
           <div className="flex flex-wrap justify-center">
-          {employees.map((employee) => {              
-              return(
+            {employees.map((employee) => {
+              return (
                 <Employee
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
-                />)
-          })}   
+                  updateEmployee={updateEmployee}
+                />
+              );
+            })}
           </div>
         </>
-        :
-          <p>You are not permitted to see the employees</p>
-        
-      }
+      ) : (
+        <p>You are not permitted to see the employees</p>
+      )}
     </div>
   );
 }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,11 +2,11 @@ import EditEmployee from "./EditEmployee";
 
 function Employee(props) {
   return (
-    <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+    <div className="w-[350px] m-2 py-8 px-8 max-w-sm bg-cyan-300 shadow-xl shadow-cyan-200/50 rounded-xl space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
       <img
         className="object-cover h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
         src={props.img}
-        alt="Employee headshot" 
+        alt="Employee headshot"
       />
       <div className="text-center space-y-2 sm:text-left">
         <div className="space-y-0.5">
@@ -15,8 +15,12 @@ function Employee(props) {
             {props.role ? props.role : "No Role"}
           </p>
         </div>
-        <EditEmployee />
-        
+        <EditEmployee
+          id={props.id}
+          name={props.name}
+          role={props.role}
+          updateEmployee={props.updateEmployee}
+        />
       </div>
     </div>
   );
